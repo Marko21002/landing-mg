a8a6a27ae9f7da4d0c4d0a7e5f83c83d017fecfe
Marko21002 <markgufman21@gmail.com>
2024-08-15 20:38:43 +0200

    test2

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -11,33 +11,33 @@ export const CallToAction = () => {
       <div className="container">
         <div className="section-heading relative">
           <h2 className="section-title">Soon in production</h2>
-          <p className="section-description">
+          {/* <p className="section-description">
             You can send us an application if you want to become content creator
             at the platform
-          </p>
-          {/* <Image
+          </p> */}
+          <Image
             src={starImage}
             alt="Star Image"
             width={200}
-            className="absolute -left-[350px] -top-[100px]"
+            className="hidden lg:block absolute -left-[350px] -top-[100px]"
           />
           <Image
             src={springImage}
             alt="Spring Image"
             width={200}
-            className="absolute -right-[300px] -top-[40px]"
-          /> */}
+            className="hidden lg:block absolute -right-[300px] -top-[40px]"
+          />
         </div>
         <div className="flex gap-2 mt-10 justify-center">
           {/* <button className="btn btn-primary">Become content creator</button> */}
 
-          <button className="btn btn-text gap-1">
+          {/* <button className="btn btn-text gap-1">
             <span>Join the waitlist</span>
             <ArrowRight className="h-5 w-5" />
-          </button>
+          </button> */}
         </div>
         <div className="flex items-center justify-center mt-10">
-          <CardDemo></CardDemo>
+          {/* <CardDemo></CardDemo> */}
         </div>
       </div>
     </section>
